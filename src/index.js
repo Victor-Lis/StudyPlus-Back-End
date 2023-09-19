8d571d0d82844f892a4153a3665ad65612085f4e
Victor-Lis <victorlisbronzo1@gmail.com>
2023-09-19 11:54:46 -0300

    v1

--- src/index.js
+++ src/index.js
@@ -335,5 +335,5 @@ schedule.scheduleJob('* * * * *', async function () {
 const port = process.env.PORT || 4000
 
 app.listen(port, () => {
-  console.log('Server is running on port 4000');
+  console.log(port == 4000? `Server is running on http://localhost:${port}`: `Server is running on ${port}`);
 });

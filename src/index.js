f59e8409b960d5a592e8b2425c840476ed3f231f
Victor-Lis <victorlisbronzo1@gmail.com>
2023-09-20 09:40:43 -0300

    v1

--- src/index.js
+++ src/index.js
@@ -8,213 +8,216 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors());
 
-async function checkNeedsCreate() {
+// async function checkNeedsCreate() {
 
-  let needsCreate = false
-  let weekCreated
-  let ano = new Date().getFullYear()
-  let mes = new Date().getMonth()
-  let dia = new Date().getDate()
+//   let needsCreate = false
+//   let weekCreated
+//   let ano = new Date().getFullYear()
+//   let mes = new Date().getMonth()
+//   let dia = new Date().getDate()
 
-  const weeks = await prisma.week.findMany({
-    orderBy: {
+//   const weeks = await prisma.week.findMany({
+//     orderBy: {
 
-      id: "desc"
+//       id: "desc"
 
-    },
-    include: {
+//     },
+//     include: {
 
-      days: {
+//       days: {
 
-        include: {
+//         include: {
 
-          tarefas: {
+//           tarefas: {
 
-            include: {
+//             include: {
 
-              Categorie: true,
+//               Categorie: true,
 
-            }
+//             }
 
-          },
+//           },
 
-        }
+//         }
 
-      },
+//       },
 
-    }
-  })
+//     }
+//   })
 
-  // console.log(`${weeks[0].days[6].date.getFullYear()} < ${ano}? ${weeks[0].days[6].date.getFullYear() < ano}`)
-  if (weeks[0].days[6].date.getFullYear() < ano) {
+//   // console.log(`${weeks[0].days[6].date.getFullYear()} < ${ano}? ${weeks[0].days[6].date.getFullYear() < ano}`)
 
-    needsCreate = true
+  
 
-  }
+//   if (weeks[0].days[6].date.getFullYear() < ano) {
 
-  // console.log(`${weeks[0].days[6].date.getMonth()} < ${mes} && ${weeks[0].days[6].date.getFullYear()} == ${ano}? ${weeks[0].days[6].date.getMonth() < mes && weeks[0].days[6].date.getFullYear() == ano}`)
-  if (weeks[0].days[6].date.getMonth() < mes && weeks[0].days[6].date.getFullYear() == ano) {
+//     needsCreate = true
 
-    // console.log(`${weeks[0].days[6].date.getMonth()} < ${mes}? ${weeks[0].days[6].date.getMonth() < mes}`)
-    needsCreate = true
+//   }
 
-  }
+//   // console.log(`${weeks[0].days[6].date.getMonth()} < ${mes} && ${weeks[0].days[6].date.getFullYear()} == ${ano}? ${weeks[0].days[6].date.getMonth() < mes && weeks[0].days[6].date.getFullYear() == ano}`)
+//   if (weeks[0].days[6].date.getMonth() < mes && weeks[0].days[6].date.getFullYear() == ano) {
 
-  console.log(`${weeks[0].days[6].date.getMonth()} < ${mes}? ${weeks[0].days[6].date.getMonth() < mes}`)  
-  if (weeks[0].days[6].date.getDate() < dia && weeks[0].days[6].date.getMonth() == mes && weeks[0].days[6].date.getFullYear() == ano) {
+//     // console.log(`${weeks[0].days[6].date.getMonth()} < ${mes}? ${weeks[0].days[6].date.getMonth() < mes}`)
+//     needsCreate = true
 
-    needsCreate = true
+//   }
 
-  }
+//   console.log(`${weeks[0].days[6].date.getMonth()} < ${mes}? ${weeks[0].days[6].date.getMonth() < mes}`)  
+//   if (weeks[0].days[6].date.getDate() < dia && weeks[0].days[6].date.getMonth() == mes && weeks[0].days[6].date.getFullYear() == ano) {
 
-  if (needsCreate) {
+//     needsCreate = true
 
-    // console.log("criou")
+//   }
 
-    weekCreated = await prisma.week.create()
+//   if (needsCreate) {
 
-    for (var i = 0; i <= 6; i++) {
+//     // console.log("criou")
 
-      let date = new Date()
-      date.setDate(dia + i)
+//     weekCreated = await prisma.week.create()
 
-      let day = await prisma.day.create({
+//     for (var i = 0; i <= 6; i++) {
 
-        data: {
+//       let date = new Date()
+//       date.setDate(dia + i)
 
-          date: date.toISOString(),
-          week: weekCreated.id,
+//       let day = await prisma.day.create({
 
-        }
+//         data: {
 
-      });
+//           date: date.toISOString(),
+//           week: weekCreated.id,
 
-    }
+//         }
 
-    weekCreated = await prisma.week.findMany({
-      orderBy: {
+//       });
 
-        id: "desc"
+//     }
 
-      },
-      include: {
+//     weekCreated = await prisma.week.findMany({
+//       orderBy: {
 
-        days: true,
+//         id: "desc"
 
-      }
-    })
+//       },
+//       include: {
 
-  }
+//         days: true,
 
-  // console.log(weekCreated)
+//       }
+//     })
 
-  if (needsCreate) {
+//   }
 
-    return weekCreated
+//   // console.log(weekCreated)
 
-  } else {
+//   if (needsCreate) {
 
-    return weeks[0]
+//     return weekCreated
 
-  }
+//   } else {
 
-}
+//     return weeks[0]
 
-async function updateTime() {
+//   }
 
-  const today = new Date();
-  // today.setHours(0, 0, 0);
-  today.setUTCHours(0, 0, 0);
-  let todayISOS = today.toISOString()
-  todayISOS = todayISOS.slice(0, 10) + 'T00:00:00.000Z'
+// }
 
-  // console.log(todayISOS)
-  // let updatedWeek;
-  let updatedDay;
-  // console.log("RODOU")
+// async function updateTime() {
 
-  const weeks = await prisma.week.findMany({
-    orderBy: {
+//   const today = new Date();
+//   // today.setHours(0, 0, 0);
+//   today.setUTCHours(0, 0, 0);
+//   let todayISOS = today.toISOString()
+//   todayISOS = todayISOS.slice(0, 10) + 'T00:00:00.000Z'
 
-      id: "desc"
+//   // console.log(todayISOS)
+//   // let updatedWeek;
+//   let updatedDay;
+//   // console.log("RODOU")
 
-    },
-    include: {
-      days: {
-        include: {
-          tarefas: true
-        },
-      },
-    },
-  });
+//   const weeks = await prisma.week.findMany({
+//     orderBy: {
 
-  if (weeks) {
-    weeks[0].days.map(async (day, index) => {
+//       id: "desc"
 
-      // console.log(`${day.date.getDate()} == ${today.getDate()+1}? ${day.date.getDate() == today.getDate()+1}    Lin: 154`)
-      if (day.date.getDate() == today.getDate()+1) {
-        console.log(day.tarefas)
+//     },
+//     include: {
+//       days: {
+//         include: {
+//           tarefas: true
+//         },
+//       },
+//     },
+//   });
 
-        day.tarefas.map(async (tarefas, index) => {
+//   if (weeks) {
+//     weeks[0].days.map(async (day, index) => {
 
-          let firstHour = Number(tarefas.primeira_hora.slice(0, 2))
-          let firstMinute = Number(tarefas.primeira_hora.slice(3, 5))
+//       // console.log(`${day.date.getDate()} == ${today.getDate()+1}? ${day.date.getDate() == today.getDate()+1}    Lin: 154`)
+//       if (day.date.getDate() == today.getDate()+1) {
+//         console.log(day.tarefas)
 
-          let lastHour = Number(tarefas.ultima_hora.slice(0, 2))
-          let lastMinute = Number(tarefas.ultima_hora.slice(3, 5))
+//         day.tarefas.map(async (tarefas, index) => {
 
-          let horaAtual = new Date().getHours()
-          let minutoAtual = new Date().getMinutes()
+//           let firstHour = Number(tarefas.primeira_hora.slice(0, 2))
+//           let firstMinute = Number(tarefas.primeira_hora.slice(3, 5))
 
-          // console.log(`${firstHour} < ${horaAtual}? ${firstHour < horaAtual}`)
-          // console.log(`${firstHour} == ${horaAtual}? ${firstHour == horaAtual} && ${firstMinute} <= ${minutoAtual}? ${firstMinute <= minutoAtual}`)
-          // console.log(`${lastHour} > ${horaAtual}? ${lastHour > horaAtual}`)
-          // console.log(`${lastHour} == ${horaAtual}? ${lastHour == horaAtual} && ${lastMinute} >= ${minutoAtual}? ${lastMinute >= minutoAtual}`)
+//           let lastHour = Number(tarefas.ultima_hora.slice(0, 2))
+//           let lastMinute = Number(tarefas.ultima_hora.slice(3, 5))
+
+//           let horaAtual = new Date().getHours()
+//           let minutoAtual = new Date().getMinutes()
+
+//           // console.log(`${firstHour} < ${horaAtual}? ${firstHour < horaAtual}`)
+//           // console.log(`${firstHour} == ${horaAtual}? ${firstHour == horaAtual} && ${firstMinute} <= ${minutoAtual}? ${firstMinute <= minutoAtual}`)
+//           // console.log(`${lastHour} > ${horaAtual}? ${lastHour > horaAtual}`)
+//           // console.log(`${lastHour} == ${horaAtual}? ${lastHour == horaAtual} && ${lastMinute} >= ${minutoAtual}? ${lastMinute >= minutoAtual}`)
 
           
-          // console.log((firstHour < horaAtual || firstHour == horaAtual && firstMinute <= minutoAtual) && (lastHour > horaAtual || lastHour == horaAtual && lastMinute >= minutoAtual))
-          if ((firstHour < horaAtual || firstHour == horaAtual && firstMinute <= minutoAtual) && (lastHour > horaAtual || lastHour == horaAtual && lastMinute >= minutoAtual)) {
+//           // console.log((firstHour < horaAtual || firstHour == horaAtual && firstMinute <= minutoAtual) && (lastHour > horaAtual || lastHour == horaAtual && lastMinute >= minutoAtual))
+//           if ((firstHour < horaAtual || firstHour == horaAtual && firstMinute <= minutoAtual) && (lastHour > horaAtual || lastHour == horaAtual && lastMinute >= minutoAtual)) {
 
-            updatedDay = index
+//             updatedDay = index
 
-            let week = weeks[0];
-            let hours = week.hours + 1.66666666667;
+//             let week = weeks[0];
+//             let hours = week.hours + 1.66666666667;
 
-            await prisma.week.update({
-              where: {
-                id: weeks[0].id,
-              },
-              data: {
-                hours: hours,
-              },
-            });
+//             await prisma.week.update({
+//               where: {
+//                 id: weeks[0].id,
+//               },
+//               data: {
+//                 hours: hours,
+//               },
+//             });
 
-            await prisma.day.update({
+//             await prisma.day.update({
 
-              where: {
+//               where: {
 
-                id: day.id,
+//                 id: day.id,
 
-              },
-              data: {
+//               },
+//               data: {
 
-                hours: day.hours + 1.66666666667,
+//                 hours: day.hours + 1.66666666667,
 
-              }
+//               }
 
-            })
+//             })
 
-          }
+//           }
 
-        })
+//         })
 
-      }
+//       }
 
-    })
-  }
-  return weeks[0]
+//     })
+//   }
+//   return weeks[0]
 
-}
+// }
 
 app.get('/weeks', async (req, res) => {
 
@@ -248,89 +251,89 @@ app.get('/update-week-minute-count', async (req, res) => {
 
 });
 
-app.post('/task-create', async (req, res) => {
+// app.post('/task-create', async (req, res) => {
 
-  const { day, categorie, title, desc, primeira_hora, ultima_hora } = req.body;
+//   const { day, categorie, title, desc, primeira_hora, ultima_hora } = req.body;
 
-  let task = await prisma.task.create({
+//   let task = await prisma.task.create({
 
-    data: {
+//     data: {
 
-      day,
-      categorie: Number(categorie),
-      title,
-      desc,
-      primeira_hora,
-      ultima_hora,
+//       day,
+//       categorie: Number(categorie),
+//       title,
+//       desc,
+//       primeira_hora,
+//       ultima_hora,
 
-    }
+//     }
 
-  })
+//   })
 
-  res.send(JSON.stringify(task))
+//   res.send(JSON.stringify(task))
 
-});
+// });
 
-app.post('/task-delete', async (req, res) => {
+// app.post('/task-delete', async (req, res) => {
 
-  const { id } = req.body;
+//   const { id } = req.body;
 
-  await prisma.task.delete({
+//   await prisma.task.delete({
 
-    where: {
+//     where: {
 
-      id,
+//       id,
 
-    }
+//     }
 
-  })
+//   })
 
-  res.sendStatus(200)
+//   res.sendStatus(200)
 
-});
+// });
 
-app.post('/categorie-create', async (req, res) => {
+// app.post('/categorie-create', async (req, res) => {
 
-  const { title, color } = req.body;
+//   const { title, color } = req.body;
 
-  let categorie = await prisma.categorie.create({
+//   let categorie = await prisma.categorie.create({
 
-    data: {
+//     data: {
 
-      title,
-      color,
+//       title,
+//       color,
 
-    }
+//     }
 
-  })
+//   })
 
-  res.send(JSON.stringify(categorie))
+//   res.send(JSON.stringify(categorie))
 
-});
+// });
 
-app.post('/categorie-delete', async (req, res) => {
+// app.post('/categorie-delete', async (req, res) => {
 
-  const { id } = req.body;
+//   const { id } = req.body;
 
-  await prisma.categorie.delete({
+//   await prisma.categorie.delete({
 
-    where: {
+//     where: {
 
-      id,
+//       id,
 
-    }
+//     }
 
-  })
+//   })
 
-  res.sendStatus(200)
+//   res.sendStatus(200)
 
-});
+// });
 
-schedule.scheduleJob('* * * * *', async function () {
+// schedule.scheduleJob('* * * * *', async function () {
 
-  updateTime()
+//   updateTime()
 
-})
+// })
 
 const port = process.env.PORT || 4000
 
